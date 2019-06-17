71f43c88c88bb7b8e80adbe4ccf950e9d158f7c1
shuvabiswas12 <shuvabiswas12@gmail.com>
2019-06-17 11:14:41 +0600

    Updated

--- script.py
+++ script.py
@@ -23,32 +23,31 @@ sleep(1)
 writer = csv.writer(open(parameters.file_name, 'w+'))
 writer.writerow(['Name', 'Job Title', 'Location', 'School', 'LinkedIn Url'])
 
+if parameters.email and parameters.password:
+    try:
+        # If login fields not found in 1st page
+        login = driver.find_element_by_xpath('//a[@class="nav__button-secondary"]')
+        login.click()
 
-try:
-    # If login fields not found in 1st page
-    login = driver.find_element_by_xpath('//a[@class="nav__button-secondary"]')
-    login.click()
+        email_field = driver.find_element_by_xpath('//input[@id="username"]')
+        password_field = driver.find_element_by_xpath('//input[@id="password"]')
+        
+        email_field.send_keys(parameters.email)
+        password_field.send_keys(parameters.password)
 
-    email_field = driver.find_element_by_xpath('//input[@id="username"]')
-    password_field = driver.find_element_by_xpath('//input[@id="password"]')
-    
-    email_field.send_keys(parameters.email)
-    password_field.send_keys(parameters.password)
+        login_btn = driver.find_element_by_xpath('//button[@type="submit"]')
+        login_btn.click()
 
-    login_btn = driver.find_element_by_xpath('//button[@type="submit"]')
-    login_btn.click()
+    except:
+        # If login fields found in 1st page
+        email_field = driver.find_element_by_xpath('//input[@class="login-email reg-field__input"]')
+        password_field = driver.find_element_by_xpath('//input[@class="login-password reg-field__input"]')
 
-except:
-    # If login fields found in 1st page
-    email_field = driver.find_element_by_xpath('//input[@class="login-email reg-field__input"]')
-    password_field = driver.find_element_by_xpath('//input[@class="login-password reg-field__input"]')
+        email_field.send_keys()
+        password_field.send_keys()
 
-    email_field.send_keys()
-    password_field.send_keys()
-
-    login_btn = driver.find_element_by_xpath('//input[@class="login submit-button"]')
-    login_btn.click()
-
+        login_btn = driver.find_element_by_xpath('//input[@class="login submit-button"]')
+        login_btn.click() 
 
 driver.get("https://www.google.com/")
 
@@ -70,14 +69,24 @@ for profile_url in linkedIn_urls:
     driver.get(profile_url)
     sleep(2)
     sel = Selector(text=driver.page_source)
-    
-    name = sel.xpath('//li[@class="inline t-24 t-black t-normal break-words"]/text()').extract_first().strip()
-    job_title = sel.xpath('//h2[@class="mt1 t-18 t-black t-normal"]/text()').extract_first().strip()
-    location = sel.xpath('//li[@class="t-16 t-black t-normal inline-block"]/text()').extract_first().strip()
-    school = sel.xpath('//li[@href="#education-section"]//span[@class="lt-line-clamp__line lt-line-clamp__line--last"]/text()').extract_first()
-    if school:
-        school = school.strip()
-    
+
+    if parameters.email and parameters.password:
+        print("\nemail found\n")
+        name = sel.xpath('//li[@class="inline t-24 t-black t-normal break-words"]/text()').extract_first().strip()
+        job_title = sel.xpath('//h2[@class="mt1 t-18 t-black t-normal"]/text()').extract_first().strip()
+        location = sel.xpath('//li[@class="t-16 t-black t-normal inline-block"]/text()').extract_first().strip()
+        school = sel.xpath('//li[@href="#education-section"]//span[@class="lt-line-clamp__line lt-line-clamp__line--last"]/text()').extract_first()
+        if school:
+            school = school.strip()
+    else:
+        print("\nemail not found\n")
+        name = sel.xpath('//div[@class="topcard__info-container"]/h1[@class="topcard__name"]/text()').extract_first().strip()
+        job_title = sel.xpath('//div[@class="topcard__info-container"]//h2[@class="topcard__headline"]/text()').extract_first().strip()
+        location = sel.xpath('//div[@class="topcard__info-container"]//h3[@class="topcard__location"]/text()').extract_first().strip()
+        school = sel.xpath('//div[@class="topcard-links--flex"]//span[@class="topcard-links__description"]/text()').extract_first()
+        if school:
+            school = school.strip()
+        
     linkedin_url = driver.current_url
 
     name = validateField(name)
@@ -92,13 +101,14 @@ for profile_url in linkedIn_urls:
     print(f'School: {school}')
     print(f'LinkedIn Url: {linkedin_url}')
 
-    writer.writerow([name.encode('utf-8'), 
-                    job_title.encode('utf-8'), 
-                    location.encode('utf-8'),
-                    school.encode('utf-8'),
-                    linkedin_url.encode('utf-8')])
+    writer.writerow([ "".join(map(chr, name.encode('utf-8'))), 
+                    "".join(map(chr, job_title.encode('utf-8'))), 
+                    "".join(map(chr, location.encode('utf-8'))),
+                    "".join(map(chr, school.encode('utf-8'))),
+                    "".join(map(chr, linkedin_url.encode('utf-8'))) ])
 
 sleep(1)
+print(f"\n------Task Complete------\n")
 driver.quit()
 
 
